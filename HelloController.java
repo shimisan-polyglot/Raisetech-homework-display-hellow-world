1938e8496ec6480c06f1f5b1bb2a456021f9a694
しみさん <74240481+shimisan-polyglot@users.noreply.github.com>
2020-11-20 13:48:32 +0900

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,17 @@
+package com.example.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("message", "課題できました");
+        return "index";
+    }
+    
+}
